1eb1cf1afcb67c83ae9f87b27c447e93328c4f7e
Kakeru Fukuda <stand.by.we@gmail.com>
2019-10-02 15:56:24 +0900

    とりあえず

---
+++ SwiftUIPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayground
+//
+//  Created by H5266 on 2019/10/02.
+//  Copyright © 2019 Kakeru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
